b258087bed31df25afe08f32e285b87f6d566dce
CaroleSa <carole220287@hotmail.com>
2020-02-07 10:32:03 +0100

    create js and css files, add debug_toolbar

--- dietetic_project/urls.py
+++ dietetic_project/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls import url
+from django.conf import settings
 from dietetic import views
 
 urlpatterns = [
     url(r'^$', views.index),
     path('admin/', admin.site.urls)
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
