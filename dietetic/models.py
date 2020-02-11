514282e09786c31c18b2f3e55834c387d615d225
CaroleSa <carole220287@hotmail.com>
2020-02-11 11:51:45 +0100

    models in dietetic app in progress

--- dietetic/models.py
+++ dietetic/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class RobotQuestion(models.Model):
+    text = models.CharField(unique=True)
+
+class RobotQuestionType(models.Model):
+    robot_question = models.ForeignKey(RobotQuestion, on_delete=models.CASCADE)
+    type = models.CharField(unique=True)
+
+class UserAnswer(models.Model):
+    robot_questions = models.ManyToManyField(RobotQuestion, through='RobotAnswer')
+    text = models.CharField()
+
+class RobotAnswer(models.Model):
+    user_answer = models.ForeignKey(UserAnswer, on_delete=models.CASCADE)
+    robot_question = models.ForeignKey(RobotQuestion, on_delete=models.CASCADE)
+    text = models.CharField()
+
+class RobotAdvices(models.Model):
+    text = models.CharField()
+
+class RobotAdviceType(models.Model):
+    robot_advices = models.ForeignKey(RobotAdvices, on_delete=models.CASCADE)
+    type = models.CharField()
+
+# clef etrangère entre robot_advices et table intermédiaire
+
+
