53743275f05ea680b6d0bd3ebadcfb8b22ab9509
CaroleSa <carole220287@hotmail.com>
2020-02-11 14:38:17 +0100

    models dietetic app ok

--- dietetic/models.py
+++ dietetic/models.py
@@ -1,28 +1,38 @@
+#! /usr/bin/env python3
+# coding: UTF-8
+
+""" Dietetic models """
+
+# imports
 from django.db import models
+
 
 class RobotQuestion(models.Model):
     text = models.CharField(unique=True)
+
 
 class RobotQuestionType(models.Model):
     robot_question = models.ForeignKey(RobotQuestion, on_delete=models.CASCADE)
     type = models.CharField(unique=True)
 
+
 class UserAnswer(models.Model):
-    robot_questions = models.ManyToManyField(RobotQuestion, through='RobotAnswer')
+    discussion_space = models.ManyToManyField(RobotQuestion, through='RobotAnswer')
     text = models.CharField()
+
 
 class RobotAnswer(models.Model):
     user_answer = models.ForeignKey(UserAnswer, on_delete=models.CASCADE)
     robot_question = models.ForeignKey(RobotQuestion, on_delete=models.CASCADE)
+    robot_advices = models.ForeignKey(RobotAdvices, on_delete=models.CASCADE)
     text = models.CharField()
 
+
 class RobotAdvices(models.Model):
+    advices_to_user = models.ManyToManyField(IdentityUser)
     text = models.CharField()
+
 
 class RobotAdviceType(models.Model):
     robot_advices = models.ForeignKey(RobotAdvices, on_delete=models.CASCADE)
     type = models.CharField()
-
-# clef etrangère entre robot_advices et table intermédiaire
-
-
