bd854736fbf3dad877ffe8a1580ef54d335e01da
CaroleSa <carole220287@hotmail.com>
2020-02-11 14:49:12 +0100

    add max_length in Dietetic models

--- dietetic/models.py
+++ dietetic/models.py
@@ -8,31 +8,32 @@ from django.db import models
 
 
 class RobotQuestion(models.Model):
-    text = models.CharField(unique=True)
+    text = models.CharField(max_length=300, unique=True)
 
 
 class RobotQuestionType(models.Model):
     robot_question = models.ForeignKey(RobotQuestion, on_delete=models.CASCADE)
-    type = models.CharField(unique=True)
+    type = models.CharField(max_length=20, unique=True)
 
 
 class UserAnswer(models.Model):
     discussion_space = models.ManyToManyField(RobotQuestion, through='RobotAnswer')
-    text = models.CharField()
+    text = models.CharField(max_length=200, unique=True)
+
+
+class RobotAdvices(models.Model):
+    # replace RobotQuestion to IdentityUser
+    advices_to_user = models.ManyToManyField(RobotQuestion)
+    text = models.CharField(max_length=500, unique=True)
 
 
 class RobotAnswer(models.Model):
     user_answer = models.ForeignKey(UserAnswer, on_delete=models.CASCADE)
     robot_question = models.ForeignKey(RobotQuestion, on_delete=models.CASCADE)
     robot_advices = models.ForeignKey(RobotAdvices, on_delete=models.CASCADE)
-    text = models.CharField()
-
-
-class RobotAdvices(models.Model):
-    advices_to_user = models.ManyToManyField(IdentityUser)
-    text = models.CharField()
+    text = models.CharField(max_length=500)
 
 
 class RobotAdviceType(models.Model):
     robot_advices = models.ForeignKey(RobotAdvices, on_delete=models.CASCADE)
-    type = models.CharField()
+    type = models.CharField(max_length=20, unique=True)
