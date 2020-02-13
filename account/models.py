abe7ee07f0fa0260425871e80177ae0dac01090b
CaroleSa <carole220287@hotmail.com>
2020-02-13 17:11:46 +0100

    html, css in progress - models account, dietetic in progress. runserver works

--- account/models.py
+++ account/models.py
@@ -4,9 +4,45 @@
 """ User models """
 
 # imports
-"""from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+"""from datetime.date import today"""
 
+"""class StatusUser(AbstractBaseUser):
+    groups =
+    user_permissions =
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
-class User(AbstractUser):
-    pass"""
+    @property
+    def is_staff(self):
+        return self.is_staff
+
+    @property
+    def is_superuser(self):
+        return self.is_superuser
+
+    @property
+    def is_active(self):
+        return self.is_active
+
+class HistoryUser(AbstractBaseUser):
+    date_joined =
+    last_login ="""
+
+"""class ProfileUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    starting_weight = models.DecimalField(max_digits=4, decimal_places=1, blank=True)
+    final_weight = models.DecimalField(max_digits=4, decimal_places=1, blank=True)
+
+class ResultsUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    weighing_date = models.DateField(default=today, blank=True)
+    weight = models.DecimalField(max_digits=4, decimal_places=1, blank=True)
+
+class IdentityUser(AbstractBaseUser):
+    username =
+    email =
+    password ="""
+
