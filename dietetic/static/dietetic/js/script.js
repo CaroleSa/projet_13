215f4079a69d10e2ecf38cf622639042daa4595e
CaroleSa <carole220287@hotmail.com>
2020-02-15 22:48:05 +0100

    logo nav color in progress

--- dietetic/static/dietetic/js/script.js
+++ dietetic/static/dietetic/js/script.js
@@ -8,3 +8,18 @@ $card.on('click', function() {
     $dietetic_text.show();
     $card.css('background-image', "url('../../static/dietetic/img/back_face.jpg')");
 });
+
+
+//     logo nav animation
+
+var $button = $('#button_nav');
+$button.on('mouseover', function(event) {
+  var $id_elt = "#"+event.target.id;
+  var $logo = $(''+$id_elt+'');
+  $logo.css('color', "rgb(30,20,200)");
+});
+$button.on('mouseleave', function(event) {
+    var $id_elt = "#"+event.target.id;
+    var $logo = $(''+$id_elt+'');
+    $logo.css('color', "rgb(0,0,0)");
+});
