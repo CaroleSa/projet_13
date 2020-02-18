78fabf6aa67a3fe320b2f7fda0c95b7ff60e0d6b
CaroleSa <carole220287@hotmail.com>
2020-02-18 14:59:10 +0100

    add linkedin and github logo

--- dietetic/static/dietetic/js/script.js
+++ dietetic/static/dietetic/js/script.js
@@ -1,12 +1,11 @@
 //     return card if the user click
 
 var $dietetic_text = $('#dietetic_text');
-$dietetic_text.hide();
-
+$dietetic_text.css('visibility', "hidden");
 var $card = $('#card');
 $card.on('click', function() {
-    $dietetic_text.show();
-    $card.css('background-image', "url('../../static/dietetic/img/back_face.jpg')");
+$dietetic_text.css('visibility', "");
+$card.css('background-image', "url('../../static/dietetic/img/back_face.jpg')");
 });
 
 
