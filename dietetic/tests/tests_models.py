b88dacc700de89f567f6f1d8246fd47e324801fa
CaroleSa <carole220287@hotmail.com>
2020-02-20 21:21:00 +0100

    10 tests account and dietetic app works ok

--- dietetic/tests/tests_models.py
+++ dietetic/tests/tests_models.py
@@ -8,7 +8,7 @@
 from unittest import TestCase
 from django.contrib.auth import get_user_model
 from django.conf import settings
-from dietetic.models import RobotQuestion, RobotQuestionType
+from dietetic.models import RobotQuestion, RobotQuestionType, RobotAdviceType, UserAnswer, RobotAdvices
 from django.db.utils import IntegrityError
 
 
@@ -37,7 +37,15 @@ class TestsModels(TestCase):
             pass
         self.robot_question_type = RobotQuestionType.objects.get(type=type)
 
-    def test_add_question_robot(self):
+        # create robot advice type
+        type = "Recette"
+        try:
+            RobotAdviceType.objects.create(type=type)
+        except IntegrityError:
+            pass
+        self.robot_advice_type = RobotAdviceType.objects.get(type=type)
+
+    def test_add_robot_question(self):
         """ Test create robot question """
         question = "Question test"
         try:
@@ -47,7 +55,7 @@ class TestsModels(TestCase):
         question_exists = RobotQuestion.objects.get(text=question)
         self.assertTrue(question_exists)
 
-    def test_add_question_type(self):
+    def test_add_robot_question_type(self):
         """ Test create robot question type """
         type = "Start questions"
         try:
@@ -56,3 +64,33 @@ class TestsModels(TestCase):
             pass
         type_exists = RobotQuestionType.objects.get(type=type)
         self.assertTrue(type_exists)
+
+    def test_add_robot_advice_type(self):
+        """ Test create robot advice type """
+        type = "Challenge"
+        try:
+            RobotAdviceType.objects.create(type=type)
+        except IntegrityError:
+            pass
+        type_exists = RobotAdviceType.objects.get(type=type)
+        self.assertTrue(type_exists)
+
+    def test_add_user_answer(self):
+        """ Test add user answer """
+        answer = "Oui"
+        try:
+            UserAnswer.objects.create(text=answer)
+        except IntegrityError:
+            pass
+        answer_exists = UserAnswer.objects.get(text=answer)
+        self.assertTrue(answer_exists)
+
+    def test_add_robot_advices(self):
+        """ Test add robot advices """
+        advice = "is an advice test"
+        try:
+            RobotAdvices.objects.create(text=advice, robot_advice_type=self.robot_advice_type)
+        except IntegrityError:
+            pass
+        advice_exists = RobotAdvices.objects.get(text=advice)
+        self.assertTrue(advice_exists)
