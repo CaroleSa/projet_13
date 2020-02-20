ddeaf2b838c9ff2ec492d874867f6214f98a3311
CaroleSa <carole220287@hotmail.com>
2020-02-20 14:41:34 +0100

    edit dietetic models in progress : issue foreignkeys, tests to redo

--- dietetic/models.py
+++ dietetic/models.py
@@ -8,13 +8,13 @@ from django.db import models
 from django.conf import settings
 
 
-class RobotQuestion(models.Model):
-    text = models.CharField(max_length=700, unique=True)
-
-
 class RobotQuestionType(models.Model):
-    robot_question = models.ForeignKey(RobotQuestion, on_delete=models.CASCADE)
     type = models.CharField(max_length=20, unique=True)
+
+
+class RobotQuestion(models.Model):
+    robot_question_type = models.ForeignKey(RobotQuestionType, on_delete=models.CASCADE)
+    text = models.CharField(max_length=700, unique=True)
 
 
 class UserAnswer(models.Model):
@@ -22,7 +22,12 @@ class UserAnswer(models.Model):
     text = models.CharField(max_length=200, null=True, unique=True)
 
 
+class RobotAdviceType(models.Model):
+    type = models.CharField(max_length=20, unique=True)
+
+
 class RobotAdvices(models.Model):
+    robot_advice_type = models.ForeignKey(RobotAdviceType, on_delete=models.CASCADE)
     advices_to_user = models.ManyToManyField(settings.AUTH_USER_MODEL)
     text = models.CharField(max_length=700, unique=True)
 
@@ -32,8 +37,3 @@ class RobotAnswer(models.Model):
     robot_question = models.ForeignKey(RobotQuestion, on_delete=models.CASCADE)
     robot_advices = models.ForeignKey(RobotAdvices, on_delete=models.CASCADE)
     text = models.CharField(max_length=700, null=True)
-
-
-class RobotAdviceType(models.Model):
-    robot_advices = models.ForeignKey(RobotAdvices, on_delete=models.CASCADE)
-    type = models.CharField(max_length=20, unique=True)
