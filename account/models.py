6274929a4df96d38fc5fe30aabfd227dfba9a33c
CaroleSa <carole220287@hotmail.com>
2020-03-22 21:09:59 +0100

    change create_superuser method ok

--- account/models.py
+++ account/models.py
@@ -26,17 +26,19 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(username, email, password, **extra_fields)
-
+    def create_superuser(self, email, username, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email=email,
+            username=username,
+            password=password,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
 
 class IdentityUser(AbstractBaseUser):
     username = models.CharField(max_length=150, validators=[UnicodeUsernameValidator()],
