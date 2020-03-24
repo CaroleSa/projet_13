275ca75cf6f5c0ad788d4f01df3c8574312ecc75
CaroleSa <carole220287@hotmail.com>
2020-03-24 21:33:28 +0100

    move is_staff is_superuser user_permissions in IdentityUser model because issue login superuser - works ok

--- account/models.py
+++ account/models.py
@@ -31,17 +31,16 @@ class UserManager(BaseUserManager):
         """
         Creates and saves a superuser with the given email and password.
         """
-        user_created = self.create_user(
+        user = self.create_user(
             email=email,
             username=username,
             password=password,
         )
-        user = StatusUser.objects.create(user=user_created)
         user.is_staff = True
         user.is_superuser = True
         user.save()
 
-        return user_created
+        return user
 
 
 class IdentityUser(AbstractBaseUser):
@@ -51,6 +50,24 @@ class IdentityUser(AbstractBaseUser):
     password = models.CharField(max_length=128, verbose_name='password')
     last_login = models.DateTimeField(blank=True, null=True, verbose_name='last login')
     advices_to_user = models.ManyToManyField(RobotAdvices)
+    is_staff = models.BooleanField(default=False, help_text='Designates whether the user '
+                                                            'can log into this admin site.',
+                                   verbose_name='staff status')
+    is_superuser = models.BooleanField(default=False,
+                                       help_text='Designates that this user has all permissions without '
+                                                 'explicitly assigning them.',
+                                       verbose_name='superuser status')
+    user_permissions = models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
+                                              related_name='user_set',
+                                              related_query_name='user',
+                                              to='auth.Permission',
+                                              verbose_name='user permissions')
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
 
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
@@ -68,18 +85,7 @@ class StatusUser(models.Model):
                                     related_query_name='user',
                                     to='auth.Group',
                                     verbose_name='groups')
-    user_permissions = models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
-                                              related_name='user_set',
-                                              related_query_name='user',
-                                              to='auth.Permission',
-                                              verbose_name='user permissions')
-    is_staff = models.BooleanField(default=False, help_text='Designates whether the user '
-                                                            'can log into this admin site.',
-                                   verbose_name='staff status')
-    is_superuser = models.BooleanField(default=False,
-                                       help_text='Designates that this user has all permissions without '
-                                                 'explicitly assigning them.',
-                                       verbose_name='superuser status')
+
     is_active = models.BooleanField(default=True, help_text='Designates whether this user should '
                                                             'be treated as active. Unselect this instead '
                                                             'of deleting accounts.',
