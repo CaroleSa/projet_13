1fcca4a5a34cba4041d4c5b7fad220847cc2e1e5
CaroleSa <carole220287@hotmail.com>
2020-03-30 22:36:45 +0200

    pylint and comments dietetic and account app ok - except static files

--- dietetic/views.py
+++ dietetic/views.py
@@ -4,16 +4,17 @@
 """ views of the dietetic app """
 
 # Imports
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.contrib.auth import get_user_model, logout
 from django.http import JsonResponse
-import datetime
+from account.models import ProfileUser, ResultsUser
 from .classes.controller import Controller
 from .classes.calculation import Calculation
-from account.models import HistoryUser, ProfileUser, ResultsUser, IdentityUser
+# pylint: disable=no-member
 
 
 def index(request):
+    """ index view """
     context = {}
 
     # USER'S DISCONNECTION AND DISPLAY THE INDEX PAGE
@@ -26,11 +27,14 @@ def index(request):
 
 
 def legal_notices(request):
+    """ legal_notices view """
     context = {}
+
     return render(request, 'dietetic/legal_notices.html', context)
 
 
 def dietetic_space(request):
+    """ dietetic_space view """
     # get data
     id_user = request.user.id
     old_robot_question = request.POST.get('question', False)
@@ -43,24 +47,28 @@ def dietetic_space(request):
                         "cruising_weight": cruising_weight, "weight_goal": weight_goal}
     user_answer = request.POST.get('answer')
 
-    # call controller_dietetic_space_view method and get context dict
+    # call controller_dietetic_space_view method
+    # and get context dict
     new_controller = Controller()
     context = new_controller.controller_dietetic_space_view(id_user, old_robot_question,
                                                             data_weight_user, user_answer,
                                                             weekly_weight)
-    print(context)
+
     return render(request, 'dietetic/dietetic_space.html', context)
 
 
 def my_results(request):
+    """ my_results view """
     # get data
     user = get_user_model()
-    id = user.objects.get(id=request.user.id)
-    final_weight = ProfileUser.objects.values_list("final_weight").get(user=id)[0]
-    starting_date = ResultsUser.objects.values_list("weighing_date").filter(user=id).order_by("weighing_date").first()[0]
-    last_date = ResultsUser.objects.values_list("weighing_date").filter(user=id).order_by("weighing_date").last()[0]
-    starting_weight = ResultsUser.objects.values_list("weight").filter(user=id).order_by("weighing_date").first()[0]
-    last_weight = ResultsUser.objects.values_list("weight").filter(user=id).order_by("weighing_date").last()[0]
+    user = user.objects.get(id=request.user.id)
+    final_weight = ProfileUser.objects.values_list("final_weight").get(user=user)[0]
+    weighing_date = ResultsUser.objects.values_list("weighing_date")
+    weight = ResultsUser.objects.values_list("weight")
+    starting_date = weighing_date.filter(user=user).order_by("weighing_date").first()[0]
+    last_date = weighing_date.filter(user=user).order_by("weighing_date").last()[0]
+    starting_weight = weight.filter(user=user).order_by("weighing_date").first()[0]
+    last_weight = weight.filter(user=user).order_by("weighing_date").last()[0]
     total_goal = float(starting_weight - final_weight)
 
     # get return Calculation class
@@ -70,24 +78,34 @@ def my_results(request):
     average_lost_weight = new_calculation.average_weight_loss(id)
     lost_weight = round(starting_weight - last_weight, 1)
 
+    # return list_data (user's results)
+    # to the graphic of the results page
     get_data = request.GET.get("get_data", "False")
     if get_data == "True":
         return JsonResponse(list_data, safe=False)
 
+    # if display_info == True
+    # display the graphic
+    # in the results page
     display_info = False
     if starting_date != last_date:
         display_info = True
 
-    context = {"starting_date": starting_date, "starting_weight": new_calculation.delete_o(starting_weight),
+    # create a context
+    context = {"starting_date": starting_date,
+               "starting_weight": new_calculation.delete_o(starting_weight),
                "total_goal": new_calculation.delete_o(total_goal),
-               "lost_percentage": lost_percentage, "average_lost_weight": new_calculation.delete_o(average_lost_weight),
-               "display_info": display_info, "final_weight": new_calculation.delete_o(final_weight),
+               "lost_percentage": lost_percentage,
+               "average_lost_weight": new_calculation.delete_o(average_lost_weight),
+               "display_info": display_info,
+               "final_weight": new_calculation.delete_o(final_weight),
                "lost_weight": new_calculation.delete_o(lost_weight)}
 
     return render(request, 'dietetic/my_results.html', context)
 
 
 def program(request):
+    """ program view """
     context = {}
 
     return render(request, 'dietetic/program.html', context)
